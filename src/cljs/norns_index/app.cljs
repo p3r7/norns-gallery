59d02a7652f4881642e0540b5fc0f4522f708ead
p3r7 <11557146+p3r7@users.noreply.github.com>
2021-06-08 21:22:24 +0200

    Don't filter by category tag as response is missing other tags

--- src/cljs/norns_index/app.cljs
+++ src/cljs/norns_index/app.cljs
@@ -65,7 +65,7 @@
       :else
       (mount-app-element [views/main-view-all]))
 
-    (dynamic-conf/get-from-wiki-js :tag category)) ; NB: gets stored in `norns-index.state/state`
+    (dynamic-conf/get-from-wiki-js)) ; NB: gets stored in `norns-index.state/state`
   )
 
 
